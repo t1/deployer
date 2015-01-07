f0eb686db34e40288ecadb48c1d4ef9ee588585a
Rüdiger zu Dohna <snackbox@sinntr.eu>
2015-01-07 15:03:20 +0100

    log jboss connect info

--- src/main/java/com/github/t1/deployer/ModelControllerClientProducer.java
+++ src/main/java/com/github/t1/deployer/ModelControllerClientProducer.java
@@ -1,18 +1,25 @@
 package com.github.t1.deployer;
 
 import java.io.IOException;
+import java.net.InetAddress;
 
 import javax.enterprise.inject.*;
+
+import lombok.extern.slf4j.Slf4j;
 
 import org.jboss.as.controller.client.ModelControllerClient;
 
 import com.github.t1.log.Logged;
 
+@Slf4j
 @Logged
 public class ModelControllerClientProducer {
     @Produces
     ModelControllerClient produceModelControllerClient() throws IOException {
-        return ModelControllerClient.Factory.create("127.0.0.1", 9999);
+        InetAddress host = InetAddress.getByName("localhost");
+        int port = 9999;
+        log.info("connect to JBoss AS on {}:{}", host, port);
+        return ModelControllerClient.Factory.create(host, port);
     }
 
     void closeModelControllerClient(@Disposes ModelControllerClient client) throws IOException {
