48f7b980d9f9340faf38c5db6e7b21604b9d597e
Rüdiger zu Dohna <snackbox@sinntr.eu>
2015-03-30 07:42:37 +0200

    audit client-ip

---
+++ src/main/java/com/github/t1/deployer/app/ClientIpMdcFilter.java
@@ -0,0 +1,30 @@
+package com.github.t1.deployer.app;
+
+import java.io.IOException;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+
+import org.jboss.logging.MDC;
+
+/** Put the IP-Address of the client into the MDC */
+@WebFilter("/*")
+public class ClientIpMdcFilter implements Filter {
+    private static final String MDC_NAME = "client-ip";
+
+    @Override
+    public void init(FilterConfig filterConfig) {}
+
+    @Override
+    public void destroy() {}
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException,
+            ServletException {
+        String remoteAddr = request.getRemoteAddr();
+        if (remoteAddr != null)
+            MDC.put(MDC_NAME, remoteAddr);
+        chain.doFilter(request, response);
+        MDC.remove(MDC_NAME);
+    }
+}
