32bb72661e4a9944707f4c628b3b9bc75e631baa
Rüdiger zu Dohna <snackbox@sinntr.eu>
2016-01-11 08:53:56 +0100

    updated package dependency test

--- src/test/java/com/github/t1/deployer/PackageDependenciesTest.java
+++ src/test/java/com/github/t1/deployer/PackageDependenciesTest.java
@@ -1,18 +1,5 @@
 package com.github.t1.deployer;
 
-import static java.util.Arrays.*;
-
-import java.util.List;
-
 import com.github.t1.testtools.AbstractPackageDependenciesTest;
 
-public class PackageDependenciesTest extends AbstractPackageDependenciesTest {
-    @Override
-    public List<Class<?>> getDependencyEntryPoints() {
-        return asList( //
-                com.github.t1.deployer.app.Deployments.class, //
-                com.github.t1.deployer.app.html.DeployerPage.class, //
-                com.github.t1.deployer.tools.HttpFilter.class //
-        );
-    }
-}
+public class PackageDependenciesTest extends AbstractPackageDependenciesTest {}
