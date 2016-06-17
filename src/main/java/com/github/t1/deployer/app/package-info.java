96b0879d56b0451f7f78a0e5bd73ab8940ea56c8
Rüdiger zu Dohna <snackbox@sinntr.eu>
2016-06-17 10:00:38 +0200

    fixed package dependencies

--- src/main/java/com/github/t1/deployer/app/package-info.java
+++ src/main/java/com/github/t1/deployer/app/package-info.java
@@ -3,6 +3,7 @@
         com.github.t1.deployer.repository.Repository.class,
         com.github.t1.deployer.model.Deployment.class,
 
+        com.fasterxml.jackson.annotation.JsonProperty.class,
         com.fasterxml.jackson.core.JsonGenerator.class,
         com.fasterxml.jackson.core.type.TypeReference.class,
         com.fasterxml.jackson.databind.ObjectMapper.class,
