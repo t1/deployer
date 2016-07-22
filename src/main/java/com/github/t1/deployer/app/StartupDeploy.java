e13e82f4de7ff3159d6c4af9c2bc956e97c4c1aa
Rüdiger zu Dohna <snackbox@sinntr.eu>
2016-07-22 09:44:57 +0200

    simplified and fixed StartupDeploy (`ManagedExecutorService` instead of retry)

--- src/main/java/com/github/t1/deployer/app/StartupDeploy.java
+++ src/main/java/com/github/t1/deployer/app/StartupDeploy.java
@@ -2,41 +2,20 @@ package com.github.t1.deployer.app;
 
 import lombok.extern.slf4j.Slf4j;
 
-import javax.annotation.PostConstruct;
+import javax.annotation.*;
 import javax.ejb.*;
+import javax.enterprise.concurrent.ManagedExecutorService;
 import javax.inject.Inject;
 
 @Startup
 @Singleton
 @Slf4j
 public class StartupDeploy {
-    private static final String BOOT_IN_PROCESS
-            = "WFLYCTL0379: System boot is in process; execution of remote management operations is not currently available";
     @Inject DeployerBoundary deployer;
 
-    @PostConstruct public void startup() {
-        log.info("--------------------------------------- startup deployer");
-        while (true) {
-            try {
-                deployer.post();
-                break;
-            } catch (RuntimeException e) {
-                String messageStack = messageStack(e);
-                if (messageStack.contains(BOOT_IN_PROCESS)) {
-                    log.info("--------------------------------------- retry startup\n{}", messageStack);
-                } else {
-                    log.error("--------------------------------------- startup FAILED\n{}", messageStack);
-                    return;
-                }
-            }
-        }
-        log.info("--------------------------------------- startup done");
-    }
+    @Resource ManagedExecutorService executorService;
 
-    private String messageStack(RuntimeException e) {
-        String messages = "";
-        for (Throwable t = e; t != null; t = t.getCause())
-            messages += "- " + t.getMessage() + "\n";
-        return messages;
+    @PostConstruct public void startup() {
+        executorService.execute(deployer::post);
     }
 }
