00ddb782ec95de76bf50ad6cd2809c228cdbd953
Rüdiger zu Dohna <snackbox@sinntr.eu>
2016-07-25 22:36:07 +0200

    disable autostart for JBoss 7 compatibility

--- src/main/java/com/github/t1/deployer/app/StartupDeploy.java
+++ src/main/java/com/github/t1/deployer/app/StartupDeploy.java
@@ -2,18 +2,14 @@ package com.github.t1.deployer.app;
 
 import lombok.extern.slf4j.Slf4j;
 
-import javax.annotation.*;
-import javax.ejb.*;
-import javax.enterprise.concurrent.ManagedExecutorService;
-import javax.inject.Inject;
-
-@Startup
-@Singleton
+// TODO JBoss 10.x: enable
+// @Startup
+// @Singleton
 @Slf4j
 public class StartupDeploy {
-    @Inject DeployerBoundary deployer;
-
-    @Resource ManagedExecutorService executorService;
-
-    @PostConstruct public void startup() { executorService.execute(deployer::post); }
+    // @Inject DeployerBoundary deployer;
+    //
+    // @Resource ManagedExecutorService executorService;
+    //
+    // @PostConstruct public void startup() { executorService.execute(deployer::post); }
 }
