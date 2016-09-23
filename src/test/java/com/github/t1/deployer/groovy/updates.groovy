7f3fac09b3a7e2fce5e184d8e6955ebb65e3f206
Rüdiger zu Dohna <snackbox@sinntr.eu>
2016-09-23 09:07:07 +0200

    updates.groovy: calculate next version to update to

--- src/test/java/com/github/t1/deployer/groovy/updates.groovy
+++ src/test/java/com/github/t1/deployer/groovy/updates.groovy
@@ -1,18 +1,13 @@
-import java.util.regex.Pattern
-
 class Updates implements Serializable {
-    private static final Pattern VERSION = Pattern.compile('\\[INFO\\] Building .* ([^ ]+)(-SNAPSHOT)?')
-    private static final Pattern UPDATE = Pattern.compile('\\[INFO\\] Updated \\$\\{(.*)\\} from (.*) to (.*)')
-
     static class Update implements Serializable {
-        String from, to;
+        String from, to
 
         @Override
         String toString() { from + " -> " + to }
     }
 
     Map<String, Update> updates = [:]
-    String version
+    String currentVersion
 
     Updates(String mvnOut) {
         String[] lines = mvnOut.split('\n')
@@ -24,23 +19,51 @@ class Updates implements Serializable {
     }
 
     private void scanForVersion(String line) {
-        if (this.version == null) {
-            def versionMatcher = VERSION.matcher(line)
-            if (versionMatcher.matches()) {
-                this.version = versionMatcher.group(1)
-            }
-        }
+        if (this.currentVersion)
+            return
+        def match = line =~ /\[INFO\] Building .* ([^ ]+)(-SNAPSHOT)?/
+        if (match)
+            this.currentVersion = match.group(1)
     }
 
     private void scanForUpdate(String line) {
-        def update = UPDATE.matcher(line)
-        if (update.matches()) {
-            updates.put(update.group(1), new Update(from: update.group(2), to: update.group(3)))
-        }
+        def match = line =~ /\[INFO\] Updated \$\{(?<name>.*)\} from (?<from>.*) to (?<to>.*)/
+        if (match)
+            updates.put(match.group('name'), new Update(from: match.group('from'), to: match.group('to')))
     }
+
+    public String updateVersion() {
+        List<Integer> ints = numeric(currentVersion)
+        int totalDiff = updates.values().
+                collect { diff(numeric(it.from), numeric(it.to)) }.
+                min()
+        if (totalDiff >= 0) {
+            while (ints.size() <= totalDiff)
+                ints += 0
+            ints[totalDiff] += 1
+            for (int i = totalDiff + 1; i < ints.size(); i++)
+                ints[i] = 0;
+        }
+        return ints.join('.')
+    }
+
+    private static int diff(List<Integer> from, List<Integer> to) {
+        for (int i = 0; i < from.size(); i++) {
+            if (from[i] > to[i])
+                throw new IllegalArgumentException('invalid version update: ' + from + " -> " + to)
+            if (from[i] < to[i])
+                return i
+        }
+        return (to.size() > from.size()) ? from.size() : Integer.MAX_VALUE
+    }
+
+    private static List<Integer> numeric(String version) {
+        (version - ~'-SNAPSHOT$').split('\\.').collect { String it -> (it.isInteger()) ? it as Integer : -1 }
+    }
+
 
     public boolean isEmpty() { updates.isEmpty() }
 
     @Override
-    String toString() { updates.toString() }
+    String toString() { "Updates in ${currentVersion}: ${updates.toString()}" }
 }
