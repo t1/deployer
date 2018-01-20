a5d4e738955f39cb5a687bfa0a9f41ba0ca2df1e
Rüdiger zu Dohna <snackbox@sinntr.eu>
2018-01-20 06:57:17 +0100

    build.sh: push to docker hub

--- src/test/docker/artifactory/build.sh
+++ src/test/docker/artifactory/build.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 
 cd "$(dirname "$0")"
+set -e
 
+export DOCKER_HUB_TAG=rdohna/artifactory-mock
+
+echo "------------------- build jar"
+export JAVA_HOME="/Library/Java/JavaVirtualMachines/1.8.x/Contents/Home"
 mvn clean install
 
-docker build --tag=artifactory-mock .
+echo "------------------- build image"
+docker build --tag=${DOCKER_HUB_TAG} .
+
+echo "------------------- push image"
+docker push ${DOCKER_HUB_TAG}
