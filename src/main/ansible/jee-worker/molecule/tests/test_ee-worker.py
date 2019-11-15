647d1e9ae90e7d4a0896a8325bc280ee306eecf4
Rüdiger zu Dohna <snackbox@sinntr.eu>
2019-11-15 08:01:48 +0100

    use health_check instead of grepping the log

--- src/main/ansible/jee-worker/molecule/tests/test_ee-worker.py
+++ src/main/ansible/jee-worker/molecule/tests/test_ee-worker.py
@@ -7,11 +7,7 @@ testinfra_hosts = testinfra.utils.ansible_runner.\
     get_hosts('all')
 
 
+# This test is quite unnecessary, as the role does the most important checks already
 def test_deployer_is_installed(host):
-    deployer_war = '/opt/wildfly/standalone/deployments/deployer.war'
-    assert host.file(deployer_war).is_file
-
-
-# TODO test reachable
-# def test_deployer_is_reachable(host):
-#     assert http localhost:8080/deployer contains something
+    marker = '/opt/wildfly/standalone/deployments/deployer.war.deployed'
+    assert host.file(marker).is_file
